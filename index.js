8b0f749961f94726e667166934e755d5e9d8e3b9
enti <enti@enti.enti>
2022-02-18 11:10:14 +0100

    classe 18/02

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ let mongo_client = require("mongodb").MongoClient;
 
 let url = "mongodb://localhost/";
 
+let db;
+
 console.log("Iniciando script mongo-http");
 
 mongo_client.connect(url, function(error, conn){
@@ -18,25 +20,53 @@ mongo_client.connect(url, function(error, conn){
 		return;
 	}
 	
-	let db = conn.db("tffhd");
-
-	let characters = db.collection("characters").find();
-
-	console.log(characters);
+	db = conn.db("tffhd");
 
 });
 
-/*
+
 
 http.createServer(function(req, res){
 	res.writeHead(200);
+	
+	if (req.url == "/"){
+		res.end();
 
-	let saludo = "<h1>hola k ase</h1>";
+		return;
+	}
 
-	res.end(saludo);
+	let col = "";
+
+	if(req.url == "/characters"){
+		col = "characters";
+	}
+	else if(req.url == "/items"){
+		col = "items";
+	}
+	else if(req.url == "/weapons"){
+		col = "weapons";
+	}
+	else{
+		return;
+	}
 
 
+	let col_data = db.collection(col).find();
+	
+	col_data.toArray(function(err, data){
+		console.log(data);
+
+		let string = JSON.stringify(data);
+
+		res.end(string);
+	});
 
 }).listen(1095);
 
-*/
+
+
+
+
+
+
+
